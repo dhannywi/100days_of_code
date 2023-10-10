5f9f4c1a0c1d553663ae3785c7b38accc86f2177
dhannywi <dwindrakusuma@gmail.com>
2023-10-09 22:15:06 -0500

    added increment and save function

--- javascript/passenger_counter/index.js
+++ javascript/passenger_counter/index.js
@@ -1,12 +1,18 @@
-let countEl = document.getElementById("count-el")
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
 let count = 0;
 // listen for clicks and increment `count` on click
 //change the `count-el` in the HTML to reflect new count
 function increment() {
     count++;
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
 
 function save() {
-    console.log(count);
+    let countStr = count + ' - ';
+    saveEl.textContent += countStr;
+    // reset count
+    count = 0;
+    countEl.textContent = count;
 }
