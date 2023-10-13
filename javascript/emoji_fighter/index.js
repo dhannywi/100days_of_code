cd7115cfd31f91f08400db3ded481311fbfe606c
dhannywi <dwindrakusuma@gmail.com>
2023-10-13 16:11:43 -0500

    add emoji fighter app

---
+++ javascript/emoji_fighter/index.js
@@ -0,0 +1,16 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+function rand_pos() {
+    return Math.floor(Math.random() * fighters.length)
+}
+
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    stageEl.textContent = `${fighters[rand_pos()]} vs ${fighters[rand_pos()]}`
+    
+})
