3a569048cacf5ee4a23a3bc1ca84b0fbb83d21e5
dhannywi <dwindrakusuma@gmail.com>
2023-10-17 12:26:43 -0500

    implemented localStorage fetching, added delete functionality

--- javascript/for_later/index.js
+++ javascript/for_later/index.js
@@ -1,24 +1,39 @@
-let myLeads = []
-const inputEl = document.querySelector("#input-el")
-const inputBtn = document.querySelector("#input-btn")
-const ulEl = document.querySelector("#ul-el")
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const ulEl = document.querySelector("#ul-el");
+
+let myLeads = [];
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+};
+
+function render(leads) {
+    let listItems = ""
+    for (let i=0; i<leads.length; i++) {
+        listItems += `
+        <li>
+            <a href="${leads[i]}" target="_blank">${leads[i]}</a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+};
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+});
 
 inputBtn.addEventListener("click", function() {
     if (inputEl.value !== "") {
         myLeads.push(inputEl.value)
         inputEl.value = ""
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
     }
-    renderLeads()
-})
-
-function renderLeads() {
-    let listItems = "" 
-    for (let i=0; i<myLeads.length; i++) {
-        listItems += `
-        <li>
-            <a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
-        </li>
-        `
-    }
-    ulEl.innerHTML = listItems
-}
+    render(myLeads)
+});
