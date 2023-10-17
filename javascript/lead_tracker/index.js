b1b25f3967c583f9215c5fd67cb99044f6fdce98
dhannywi <dwindrakusuma@gmail.com>
2023-10-16 21:10:03 -0500

    first iteration of chrome extension

--- javascript/lead_tracker/index.js
+++ javascript/lead_tracker/index.js
@@ -5,14 +5,20 @@ const ulEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function() {
     if (inputEl.value !== "") {
-        myLeads.push(inputEl.value)}
+        myLeads.push(inputEl.value)
+        inputEl.value = ""
+    }
     renderLeads()
 })
 
 function renderLeads() {
     let listItems = "" 
     for (let i=0; i<myLeads.length; i++) {
-        listItems += `<li>${myLeads[i]}</li>`
+        listItems += `
+        <li>
+            <a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+        </li>
+        `
     }
     ulEl.innerHTML = listItems
 }
